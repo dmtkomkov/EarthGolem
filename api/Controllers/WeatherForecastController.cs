9ffd04760b27be95d22e4beb6128e831b0fea2db
Dmitry Komkov <dmtkomkov@gmail.com>
2024-12-31 17:41:14 +0100

    123

--- api/Controllers/WeatherForecastController.cs
+++ api/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace LangWebApp.Controllers;
+namespace api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
--- api/WeatherForecast.cs
+++ api/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace LangWebApp;
+namespace api;
 
 public class WeatherForecast
 {
