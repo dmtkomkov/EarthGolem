df49ee796430350823dbce9f9956fb18cde508d2
Dmitry Komkov <dmtkomkov@gmail.com>
2025-01-14 14:26:19 +0100

    Added authorization

--- TimeTracker/Data/ApplicationDbContext.cs
+++ TimeTracker/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using TimeTracker.Models;
 
 namespace TimeTracker.Data;
 
-public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext(options)
 {
     public DbSet<Step> Steps { get; set; }
 }
--- TimeTracker/Program.cs
+++ TimeTracker/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NSwag.AspNetCore;
 using TimeTracker.Data;
@@ -16,9 +17,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddScoped<IStepRepository, StepRepository>();
 
 var app = builder.Build();
+
+//===================================================================================================================//
 
 if (app.Environment.IsDevelopment())
 {
@@ -30,5 +37,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
+
+app.MapGroup("/auth").WithTags("Auth").MapIdentityApi<IdentityUser>();
 
 app.Run();
