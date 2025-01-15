cff7193508284efcd05366d9ee4fdea91889346a
Dmitry Komkov <dmtkomkov@gmail.com>
2025-01-15 02:15:31 +0100

    Refactor Program.cs

--- TimeTracker/Program.cs
+++ TimeTracker/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NSwag.AspNetCore;
+using TimeTracker;
 using TimeTracker.Data;
 using TimeTracker.Interfaces;
 using TimeTracker.Repositories;
@@ -27,54 +28,7 @@ var app = builder.Build();
 
 if (args.Length > 0 && args[0] == "create-user")
 {
-    using var scope = app.Services.CreateScope();
-    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-    
-    var username = "admin";
-    var password = "Admin@123";
-    var email = "admin@example.com";
-    
-    for (int i = 1; i < args.Length; i++)
-    {
-        if (args[i] == "--username" && i + 1 < args.Length)
-        {
-            username = args[++i];
-        }
-        else if (args[i] == "--password" && i + 1 < args.Length)
-        {
-            password = args[++i];
-        }
-        else if (args[i] == "--email" && i + 1 < args.Length)
-        {
-            email = args[++i];
-        }
-    }
-
-    var user = await userManager.FindByNameAsync(username);
-    if (user != null)
-    {
-        Console.WriteLine("User already exists.");
-        return;
-    }
-
-    var result = await userManager.CreateAsync(new IdentityUser
-    {
-        UserName = username,
-        Email = email
-    }, password);
-
-    if (result.Succeeded)
-    {
-        Console.WriteLine("User was created successfully.");
-    }
-    else
-    {
-        foreach (var e in result.Errors)
-        {
-            Console.WriteLine($"Error: {e.Description}");
-        }
-    }
-    
+    await UserCreator.CreateUserAsync(app.Services, args);
     return;
 }
 
