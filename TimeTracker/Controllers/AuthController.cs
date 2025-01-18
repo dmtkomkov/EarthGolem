b4e009958b0537b9613a828b94cd3f0e3635af67
Dmitry Komkov <dmtkomkov@gmail.com>
2025-01-18 13:47:38 +0100

    Added token refresh

--- TimeTracker/Controllers/AuthController.cs
+++ TimeTracker/Controllers/AuthController.cs
@@ -3,7 +3,6 @@ using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 namespace TimeTracker.Controllers;
@@ -18,7 +17,7 @@ public class AuthController(
 {
     private const string RefreshTokenName = "RefreshToken";
     private const string LoginProvider = "TimeTrackerProvider";
-    private readonly SymmetricSecurityKey _securityKey = new(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!));
+    private readonly SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!));
     
     public class LoginDto
     {
@@ -65,54 +64,59 @@ public class AuthController(
         });
     }
 
-    // [HttpPost("refresh")]
-    // public async Task<IActionResult> Refresh([FromBody] RefreshTokenDto refreshRequest)
-    // {
-    //     if (!ValidateRefreshToken(refreshRequest.RefreshToken, out var _))
-    //         return Unauthorized("Invalid refresh token.");
-    //
-    //     var user = await userManager.Users.FirstOrDefaultAsync(u =>
-    //         userManager.GetAuthenticationTokenAsync(u, LoginProvider, RefreshTokenName).Result == refreshRequest.RefreshToken);
-    //
-    //     if (user == null)
-    //         return Unauthorized("User associated with the refresh token was not found.");
-    //     
-    //     var storedRefreshToken = await userManager.GetAuthenticationTokenAsync(
-    //         user,
-    //         LoginProvider,
-    //         RefreshTokenName
-    //     );
-    //
-    //     if (storedRefreshToken == null || storedRefreshToken != refreshRequest.RefreshToken)
-    //     {
-    //         return Unauthorized("Invalid refresh token.");
-    //     }
-    //
-    //     var newJwtToken = GenerateJwtToken(user);
-    //     var newRefreshTokenValue = GenerateRefreshToken(user);
-    //
-    //     var setResult = await userManager.SetAuthenticationTokenAsync(
-    //         user,
-    //         LoginProvider,
-    //         RefreshTokenName,
-    //         newRefreshTokenValue
-    //     );
-    //
-    //     if (!setResult.Succeeded)
-    //     {
-    //         return StatusCode(500, "Error updating refresh token.");
-    //     }
-    //
-    //     return Ok(new
-    //     {
-    //         Token = newJwtToken,
-    //         RefreshToken = newRefreshTokenValue
-    //     });
-    // }
+    [HttpPost("refresh")]
+    public async Task<IActionResult> Refresh([FromBody] RefreshTokenDto refreshRequest)
+    {
+        if (!ValidateRefreshToken(refreshRequest.RefreshToken, out var jwtToken) || jwtToken == null)
+            return Unauthorized("Invalid refresh token.");
+    
+        var userId = jwtToken.Claims
+            .FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Sub)?.Value;
+        
+        if (string.IsNullOrEmpty(userId))
+            return Unauthorized($"sub {userId} not found in refresh token.");
+        
+        var user = await userManager.FindByIdAsync(userId);
+        if (user == null) {
+            return Unauthorized("User associated with the refresh token was not found.");
+        }
+
+        var storedRefreshToken = await userManager.GetAuthenticationTokenAsync(
+            user,
+            LoginProvider,
+            RefreshTokenName
+        );
+    
+        if (storedRefreshToken == null || storedRefreshToken != refreshRequest.RefreshToken)
+        {
+            return Unauthorized("Invalid refresh token.");
+        }
+    
+        var newJwtToken = GenerateJwtToken(user);
+        var newRefreshTokenValue = GenerateRefreshToken(user);
+    
+        var setResult = await userManager.SetAuthenticationTokenAsync(
+            user,
+            LoginProvider,
+            RefreshTokenName,
+            newRefreshTokenValue
+        );
+    
+        if (!setResult.Succeeded)
+        {
+            return StatusCode(500, "Error updating refresh token.");
+        }
+    
+        return Ok(new
+        {
+            Token = newJwtToken,
+            RefreshToken = newRefreshTokenValue
+        });
+    }
 
     private string GenerateJwtToken(IdentityUser user)
     {
-        var signingCredentials = new SigningCredentials(_securityKey, SecurityAlgorithms.HmacSha256);
+        var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
         var expireMinutes = Convert.ToDouble(configuration["Jwt:ExpireMinutes"]);
         var claims = new List<Claim> {
             new (JwtRegisteredClaimNames.Sub, user.Id)
@@ -130,7 +134,7 @@ public class AuthController(
 
     private string GenerateRefreshToken(IdentityUser user)
     {
-        var signingCredentials = new SigningCredentials(_securityKey, SecurityAlgorithms.HmacSha256);
+        var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
         var expireDays = Convert.ToDouble(configuration["Jwt:expireDays"]);
         var claims = new List<Claim> {
             new (JwtRegisteredClaimNames.Sub, user.Id)
@@ -139,35 +143,28 @@ public class AuthController(
         var token = new JwtSecurityToken(
             claims: claims,
             notBefore: DateTime.UtcNow,
-            expires: DateTime.UtcNow.AddDays(expireDays),
+            expires: DateTime.UtcNow.AddMinutes(5),
             signingCredentials: signingCredentials
         );
 
         return new JwtSecurityTokenHandler().WriteToken(token);
     }
 
-    private bool ValidateRefreshToken(string token, out DateTime creationDate)
-    {
-        creationDate = DateTime.MinValue;
-
+    private bool ValidateRefreshToken(string token, out JwtSecurityToken? jwtToken) {
+        jwtToken = null;
         var tokenHandler = new JwtSecurityTokenHandler();
 
         try {
             tokenHandler.ValidateToken(token, new TokenValidationParameters {
                 ValidateIssuerSigningKey = true,
-                IssuerSigningKey = _securityKey,
+                IssuerSigningKey = securityKey,
                 ValidateIssuer = false,
                 ValidateAudience = false,
                 ValidateLifetime = true,
                 ClockSkew = TimeSpan.Zero
             }, out var validatedToken);
 
-            if (validatedToken is JwtSecurityToken jwtToken && 
-                jwtToken.Payload.TryGetValue(JwtRegisteredClaimNames.Iat, out var iatValue))
-            {
-                var iatSeconds = Convert.ToInt64(iatValue);
-                creationDate = DateTimeOffset.FromUnixTimeSeconds(iatSeconds).UtcDateTime;
-            }
+            jwtToken = validatedToken as JwtSecurityToken;
 
             return true;
         } catch (SecurityTokenExpiredException) {
